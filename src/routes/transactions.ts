63ad68ae1875d1e34fc9ff13a533cfd076625df0
Drarkors <rafaelfzanetti@hotmail.com>
2024-09-17 21:26:24 -0300

    🔗 Creating transactionRoutes plugin

---
+++ src/routes/transactions.ts
@@ -0,0 +1,13 @@
+import type { FastifyInstance } from 'fastify'
+
+import { knex } from '../database'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/hello', async () => {
+    const transactions = await knex('transactions')
+      .where('amount', 1000.5)
+      .select('*')
+
+    return transactions
+  })
+}
