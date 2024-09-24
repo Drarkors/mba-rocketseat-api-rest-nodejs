1fbfc11763226b7f980a5e40e2a8e77156880e03
Drarkors <rafaelfzanetti@hotmail.com>
2024-09-23 22:47:24 -0300

    🧪 Testing list transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { beforeAll, afterAll, describe, it } from 'vitest'
+import { beforeAll, afterAll, describe, it, expect } from 'vitest'
 import request from 'supertest'
 
 import { app } from '../src/app'
@@ -21,5 +21,31 @@ describe('Transaction routes', async () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transcactions from a session', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 500,
+        type: 'credit',
+      })
+      .expect(201)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies ?? [])
+      .expect(200)
+
+    expect(listTransactionsResponse.body).toHaveProperty('transactions')
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 500,
+      }),
+    ])
   })
 })
